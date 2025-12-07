f0a1044e22e28965f7e908f45c72c9b4b3297bb4
infocontacttg2023-afk <infocontacttg2023@gmail.com>
2025-12-07 17:54:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from aiogram import Bot, Dispatcher, executor, types
 import os
 
-TOKEN = os.getenv("BOT_TOKEN")
+# Беремо токен з Render / Environment Variables
+TOKEN = os.getenv("7902096104:AAEv0NPY9UEqDCA1dYMntexql294iMI_zu8")
+
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+# Коли хтось пише текст/кидає фото/відео/документ у групі
 @dp.message_handler(content_types=['text', 'photo', 'video', 'document'])
-async def auto_buttons(message: types.Message):
-    if message.chat.type in ["group", "supergroup"]:
+async def add_buttons(message: types.Message):
+    if message.chat.type in ['group', 'supergroup']:
         keyboard = types.InlineKeyboardMarkup()
         keyboard.add(
-            types.InlineKeyboardButton("👍 Лайк", callback_data="like")
+            types.InlineKeyboardButton("👍 Лайк", callback_data="like"),
+            types.InlineKeyboardButton("💬 Комент", callback_data="comment")
         )
-        await message.reply("Натисни:", reply_markup=keyboard)
+        await message.reply("Оберіть дію:", reply_markup=keyboard)
 
-@dp.callback_query_handler()
-async def c(callback_query: types.CallbackQuery):
-    await callback_query.answer("Готово!")
+# Обробка натискань
+@dp.callback_query_handler(lambda c: c.data in ['like', 'comment'])
+async def process_callback(call: types.CallbackQuery):
+    if call.data == "like":
+        await call.answer("Ти поставив лайк 👍")
+    elif call.data == "comment":
+        await call.answer("Ти хочеш залишити коментар 💬")
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
